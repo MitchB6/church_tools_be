f362024bb25552afcda8a0859f85be9bb560b4fe
iSrg <mdb4@njit.edu>
2024-05-13 01:21:06 -0400

    working, not connected

---
+++ src/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+@app.route('/hello')
+def hello_world():
+  return 'Hello, World!'
+
+if __name__ == '__main__':
+  app.run()
