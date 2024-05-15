138c9959d1dbb1ad84a85ecf8cc297a201b01a52
iSrg <mdb4@njit.edu>
2024-05-15 01:28:07 -0400

    working with front end, database implementation needed

--- src/__init__.py
+++ src/__init__.py
@@ -2,10 +2,11 @@ from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/hello')
 def hello_world():
   return 'Hello, World!'
 
 if __name__ == '__main__':
-  app.run()
+  app.run(port=5000)
